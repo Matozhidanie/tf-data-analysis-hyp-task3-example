84d828120bb755dc764d88fa4b0711ce7933325b
Matozhidanie <129894969+Matozhidanie@users.noreply.github.com>
2023-04-13 16:19:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
+chat_id = 481557740 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(hist: np.array, new: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    # уровень значимости
+    uroven = 0.06
+
+    # при гипотезе принимаем p = p0 (роста при новом способе нет)
+    # тестируем отклонение вверх (есть ли рост?) - альтернатива правосторонняя
+
+    # гипотеза p<=p0
+    # альтернатива p>p0
+
+    # вычисления p-value етодом T-test 
+    result = stats.ttest_ind (a=hist, b=new)
+
+    #print(result)
+
+    if (result.pvalue < uroven) and (result.statistic < 0):
+        otvet = bool(1)
+    else:
+        otvet = bool(0)
+
+    return otvet # Ваш ответ, True или False
